65f98d24d2ef7d77e91676776193e58dff1ada2c
Greg Doud <gpdoud@gmail.com>
2021-02-15 09:05:57 -0500

    Add project files.

---
+++ SumNbrsOneToSeventyDivisibleBySeven/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SumNbrsOneToSeventyDivisibleBySeven {
+    class Program {
+        static void Main(string[] args) {
+
+            var sum = 0;
+
+            for(var idx = 1; idx <= 70; idx++) {
+
+                if(idx % 7 == 0) {
+                    sum += idx;
+                }
+
+            }
+
+            Console.WriteLine($"The sum is {sum}");
+        }
+    }
+}
